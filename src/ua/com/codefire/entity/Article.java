d0943cf2c68510d9f22674a5b31717d143219168
glib2002 <glebix@ukr.net>
2016-06-30 18:09:24 +0300

    home work

---
+++ src/ua/com/codefire/entity/Article.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2016 CodeFireUA <edu@codefire.com.ua>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ua.com.codefire.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author CodeFireUA <edu@codefire.com.ua>
+ */
+@Entity
+@Table(name = "articles")
+public class Article implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    @Lob
+    @Column(name = "content")
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "timstamp")
+    private Date timstamp;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @ManyToMany
+    @JoinTable(name = "articles_has_categories", joinColumns = {
+        @JoinColumn(name = "article_id", referencedColumnName = "id")
+    }, inverseJoinColumns = {
+        @JoinColumn(name = "category_id", referencedColumnName = "id")
+    })
+    private List<Category> categories;
+
+    public Article() {
+    }
+
+    public Article(String title, String content, Date timstamp) {
+        this.title = title;
+        this.content = content;
+        this.timstamp = timstamp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimstamp() {
+        return timstamp;
+    }
+
+    public void setTimstamp(Date timstamp) {
+        this.timstamp = timstamp;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Article other = (Article) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" + "id=" + id + ", title=" + title + ", content=" + content + ", timstamp=" + timstamp + '}';
+    }
+
+}
